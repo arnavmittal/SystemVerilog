977e21c007cc886d22aaf322dd4f907df45dcaca
Arnav Mittal <mittal13@purdue.edu>
2016-02-25 11:31:09 -0500

    [WORKING] tb_eop_detecte done but testing it.

--- Lab6/source/tb_eop_detect.sv
+++ Lab6/source/tb_eop_detect.sv
@@ -9,6 +9,79 @@
 
 `timescale 1ns / 100ps
 
+module tb_eop_detect ();
+
+	localparam	CLK_PERIOD	= 10.4;
+	localparam	CHECK_DELAY 	= 1; // Check 1ns after the rising edge to allow for propagation delay
+
+	reg tb_clk;
+	reg tb_eop;
+	reg tb_d_minus;
+	reg tb_d_plus;
+
+	eop_detect DUT
+	(
+		.n_eop  (tb_eop),
+		.d_minus (tb_d_minus),
+		.d_plus (tb_d_plus)
+	);
+	
+	// Clock generation block
+	always
+	begin
+		tb_clk = 1'b0;
+		#(CLK_PERIOD/2.0);
+		tb_clk = 1'b1;
+		#(CLK_PERIOD/2.0);
+	end	
+	initial
+	begin
+		/////////////////////////////////////////////////////////////////////////////////////////////RESET CASE START
+		@(negedge tb_clk);
+		tb_d_plus = 1'b0;
+		tb_d_minus = 1'b0;
+		@(negedge tb_clk);
+		
+		if((tb_d_eop == '1))
+			$info("-------------------TESTCASE 1 passed");
+		else
+			$info("-------------------TESTCASE 1 failed");
+		@(negedge tb_clk);
+		
+		tb_d_plus = 1'b0;
+		tb_d_minus = 1'b1;
+		@(negedge tb_clk);
+		
+		if((tb_d_eop == '0))
+			$info("-------------------TESTCASE 2 passed");
+		else
+			$info("-------------------TESTCASE 2 failed");
+		@(negedge tb_clk);
+
+		tb_d_plus = 1'b1;
+		tb_d_minus = 1'b0;
+		@(negedge tb_clk);
+		
+		if((tb_d_eop == '0))
+			$info("-------------------TESTCASE 3 passed");
+		else
+			$info("-------------------TESTCASE 3 failed");
+		@(negedge tb_clk);
+
+		tb_d_plus = 1'b1;
+		tb_d_minus = 1'b1;
+		@(negedge tb_clk);
+		
+		if((tb_d_eop == '0))
+			$info("-------------------TESTCASE 4 passed");
+		else
+			$info("-------------------TESTCASE 4 failed");
+		@(negedge tb_clk);
+	end
+endmodule
+		/////////////////////////////////////////////////////////////////////////////////////////////RESET CASE END
+/*`timescale 1ns / 100ps
+
 module tb_eop_detect	();
 
 	//	Default Config Test Variables and Constants
@@ -180,3 +253,4 @@ module tb_eop_detect	();
 	end
 
 endmodule 
+*/
