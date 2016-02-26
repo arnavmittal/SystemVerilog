3c8a6ecdacda3b24ca18a5a67a86836880e9550c
Arnav Mittal <mittal13@purdue.edu>
2016-02-26 11:31:21 -0500

    Phase 2 work Started [WORKING] shift_register.sv

---
+++ Lab6/source/shift_register.sv
@@ -0,0 +1,8 @@
+// $Id: $
+// File name:   shift_register.sv
+// Created:     2/26/2016
+// Author:      Arnav Mittal
+// Lab Section: 337-05
+// Version:     1.0  Initial Design Entry
+// Description: This is the module for the Shift register of the USB Receiver Lab
+//              which hold the 1 byte of data to be stored in the FIFO.
