8b6ae93c9e2b272ec88cbe66c171fad81b2b6b28
Arnav Mittal <mittal13@purdue.edu>
2016-02-29 22:33:32 -0500

    [WORKING] made changes to rcu.sv | assigned outputs ||| still have to make RCU module call | [TODO] all test benches

--- Lab6/source/rcu.sv
+++ Lab6/source/rcu.sv
@@ -42,51 +42,51 @@ module rcu
 		case(state)
 			IDLE:
 			begin
-					if (d_edge == 1'b1)
-					begin
-							next_state = START;
-					end
-					else:
-					begin
-							next_state = IDLE;
-					end
+				if (d_edge == 1'b1)
+				begin
+						next_state = START;
+				end
+				else:
+				begin
+						next_state = IDLE;
+				end
 			end
 			START:
 			begin
-					if (byte_received == 1'b1)
-					begin
-							next_state = CHECK;
-					end
-					else:
-					begin
-							next_state = START;
-					end
+				if (byte_received == 1'b1)
+				begin
+						next_state = CHECK;
+				end
+				else:
+				begin
+						next_state = START;
+				end
 			end
 			CHECK:
 			begin
-					if (rcv_data == SYNC)
-					begin
-							next_state = RCV_START;
-					end
-					else:
-					begin
-							next_state = ERR_EOP_1
-					end
+				if (rcv_data == SYNC)
+				begin
+						next_state = RCV_START;
+				end
+				else:
+				begin
+						next_state = ERR_EOP_1
+				end
 			end
 			RCV_START:
 			begin
-					if (byte_received == 1'b1)
-					begin
-							next_state = RCV_DONE;
-					end
-					else if(shift_enable == 1'b1 && eop == 1'b1)
-					begin
-							next_state = EIDLE;
-					end
-					else:
-					begin
-							next_state = RCV_START;
-					end
+				if (byte_received == 1'b1)
+				begin
+						next_state = RCV_DONE;
+				end
+				else if(shift_enable == 1'b1 && eop == 1'b1)
+				begin
+						next_state = EIDLE;
+				end
+				else:
+				begin
+						next_state = RCV_START;
+				end
 			end
 			RCV_DONE:
 			begin
@@ -94,57 +94,63 @@ module rcu
 			end
 			EOP_1:
 			begin
-					if (shift_enable == 1'b1 && eop == 1'b0)
-					begin
-							next_state = RCV_START;
-					end
-					else if(shift_enable == 1'b1 && eop == 1'b1)
-					begin
-							next_state = EOP_2;
-					end
+				if (shift_enable == 1'b1 && eop == 1'b0)
+				begin
+						next_state = RCV_START;
+				end
+				else if(shift_enable == 1'b1 && eop == 1'b1)
+				begin
+						next_state = EOP_2;
+				end
 			end
 			EOP_2:
 			begin
-					if (shift_enable == 1'b1 && eop == 1'b1)
-					begin
-							next_state = IDLE;
-					end
-					else if(shift_enable == 1'b1)
-					begin
-							next_state = RCV_START;
-					end
+				if (shift_enable == 1'b1 && eop == 1'b1)
+				begin
+						next_state = IDLE;
+				end
+				else if(shift_enable == 1'b1)
+				begin
+						next_state = RCV_START;
+				end
 			end
 			ERR_EOP_1:
 			begin
-					if (shift_enable == 1'b1 && eop == 1'b1)
-					begin
-							next_state = ERR_EOP_2;
-					end
-					else
-					begin
-							next_state = ERR_EOP_1;
-					end
+				if (shift_enable == 1'b1 && eop == 1'b1)
+				begin
+						next_state = ERR_EOP_2;
+				end
+				else
+				begin
+						next_state = ERR_EOP_1;
+				end
 			end
 			ERR_EOP_2:
 			begin
-					if (shift_enable == 1'b1 && eop == 1'b0)
-					begin
-							next_state = EIDLE;
-					end
-					else
-					begin
-							next_state = ERR_EOP_2;
-					end
+				if (shift_enable == 1'b1 && eop == 1'b0)
+				begin
+						next_state = EIDLE;
+				end
+				else
+				begin
+						next_state = ERR_EOP_2;
+				end
 			end
 			EIDLE:
 			begin
-					if (d_edge == 1'b1)
-					begin
-							next_state = START;
-					end
-					else
-					begin
-							next_state = EIDLE;
-					end
-
+				if (d_edge == 1'b1)
+				begin
+						next_state = START;
+				end
+				else
+				begin
+						next_state = EIDLE;
+				end
 			end
+		endcase
+	end
+	//{IDLE, START, CHECK, RCV_START, RCV_DONE, EOP_1, EOP_2, ERR_EOP_1, ERR_EOP_2, EIDLE} 
+	assign rcving = (state == START) || (state == CHECK) || (state == RCV_START) || (state == RCV_DONE) || (state == EOP_1) || (state == EOP_2) || (state == ERR_EOP_1) || (state == ERR_EOP_2);
+	assign w_enable = (state == RCV_DONE)
+	assign r_error = (state == ERR_EOP_1) || (state == ERR_EOP_2) || (state == EIDLE);
+endmodule 
